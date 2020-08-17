21535a3aad6dddd51fb032983b88838f81fefc12
loulunds <lousanne.lundstrom@gmail.com>
2020-08-17 12:34:45 +0000

    textbox with reload made

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
@@ -11,26 +12,36 @@ messages = []
 def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """Get all messages and separate them with a `br`"""
-    return "<br>".join(messages)
-
-
-
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions(docstring)"""
-    return render_template("index,html")
-    
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
 
 
-@app.route('/<username>')
+
+@app.route('/<username>', methods =["GET", "POST"])
 def user(username):
     """Display chat message"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username = username,
+        chat_messages = messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
