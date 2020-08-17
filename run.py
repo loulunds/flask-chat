b19ebc95de0da6d2f5393add6ac41dc71ad43c07
loulunds <lousanne.lundstrom@gmail.com>
2020-08-17 09:12:15 +0000

    index.html

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 
 app = Flask(__name__)
@@ -9,7 +10,8 @@ messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
@@ -21,7 +23,8 @@ def get_all_messages():
 @app.route('/')
 def index():
     """Main page with instructions(docstring)"""
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index,html")
+    
 
 
 @app.route('/<username>')
