719d4358e5d792681c1428c88943fd659343d114
loulunds <lousanne.lundstrom@gmail.com>
2020-08-17 12:58:23 +0000

    Added chatextbox funtionality and refactored code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
@@ -9,11 +9,10 @@ messages = []
 
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
-    now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
+    now = datetime.now().strftime("%H:%M:%S") 
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
 @app.route('/', methods = ["GET", "POST"])
@@ -24,30 +23,26 @@ def index():
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user", username = session["username"]
+        ))
 
     return render_template("index.html")
 
 
 
-@app.route('/<username>', methods =["GET", "POST"])
+@app.route('/chat/<username>', methods =["GET", "POST"])
 def user(username):
-    """Display chat message"""
+    """Add and display chat messages"""
 
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
-        add_messages(username, message)
-        return redirect(session["username"])
+        add_message(username, message)
+        return redirect(url_for("user", username = session["username"]
+        ))
 
     return render_template("chat.html", username = username,
         chat_messages = messages)
-
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    """Create a new message and redirect to chat page"""
-    add_messages(username, message)
-    return redirect("/" + username)
 
 
 app.run(os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
